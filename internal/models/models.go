18ccca5199098277280935e19ffcb9471966584e
ai-redone <ridwanibrahim97@hotmail.com>
2024-02-28 13:14:37 +0100

    crates url table and model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,9 @@
 package models
+
+import "time"
+
+type Url struct {
+	ShortenedUrl string    `json:"shortened_url"`
+	OriginalUrl  string    `json:"original_url"`
+	CreatedAt    time.Time `json:"created_at"`
+}
